8287ba940934506a5451ef6fcc3df6a5cc2d3e94
Ham I-sik(a.k.a 2sik, bis) <ham2sik@gmail.com>
2022-11-21 14:30:15 +0900

    Create Theme.js

---
+++ styles/Theme.js
@@ -0,0 +1,10 @@
+const theme = {
+  colors: {
+    primary: "#111",
+    secondary: "#666",
+    dimmed: `rgba(0, 0, 0, 0.5)`,
+    dimmedClear: `rgba(0, 0, 0, 0)`,
+  },
+};
+
+export default theme;
